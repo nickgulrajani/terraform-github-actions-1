9d7dddd3798023e2d58a847be3b0985ffed34658
Nicholas Gulrajani <nicholas.gulrajani@cdw.com>
2022-09-26 11:30:41 -0400

    WIP

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-eastt-1"
+  region = "us-east-1"
   assume_role {
     role_arn     = "arn:aws:iam::${var.account_id[terraform.workspace]}:role/${var.assume_role}"
     session_name = "example"
